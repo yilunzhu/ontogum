703f8d1f34f2c99d6f73c67c28b2fdb373bf472d
yilunzhu <allen.yl.zhu@gmail.com>
2021-01-17 17:03:17 +0800

    count

--- utils/count.py
+++ utils/count.py
@@ -1,6 +1,7 @@
 import io, os
 
 num_cluster = 0
+num_mention = 0
 
 file_dir = '..' + os.sep + 'predicted' + os.sep + 'dcoref' + os.sep + 'conll'
 for split in os.listdir(file_dir):
@@ -19,9 +20,11 @@ for split in os.listdir(file_dir):
                 for coref in coref_fields:
                     if '(' in coref:
                         cluster = coref.strip('(').strip(')')
+                        num_mention += 1
                         if cluster not in existed_cluster:
                             existed_cluster.append(cluster)
                             file_cluster += 1
         num_cluster += file_cluster
 
-print(num_cluster)
+print(f'Number of coref chains: {num_cluster}')
+print(f'Number of mentions: {num_mention}')
